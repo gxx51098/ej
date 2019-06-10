a915b14c74b6b5fc9625608189d15bd2388d97fc
gxx51098 <gxx51098@163.com>
2019-06-10 16:02:07 +0800

    e洁家政服务平台初始化

---
+++ src/main/java/com/briup/apps/ej/utils/Message.java
@@ -0,0 +1,40 @@
+package com.briup.apps.ej.utils;
+
+public class Message {
+    private Integer status;
+    private String message;
+    private Object date;
+    private Long timestamp;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getDate() {
+        return date;
+    }
+
+    public void setDate(Object date) {
+        this.date = date;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
